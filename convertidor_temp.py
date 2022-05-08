80e6bc8d1f76c915d37a40f4ca951a75f43d3ae0
Christian-Coria <desbloqueoenlinea@gmail.com>
2022-05-08 01:28:19 -0300

    convertidor_temp

---
+++ convertidor_temp.py
@@ -0,0 +1,13 @@
+celcius = float(input("introduce los grados celcius. "))
+
+def celcius_a_fahrenheit(celcius):
+    return celcius *1.8 +32
+
+print(celcius_a_fahrenheit(celcius))
+
+
+fahrenheit = float(input("introduce los grados fahrenheit. "))
+def fahrenheith_a_celcius(fahrenheit):
+    return fahrenheit - 32 * 0.5556
+
+print(fahrenheith_a_celcius(fahrenheit))
